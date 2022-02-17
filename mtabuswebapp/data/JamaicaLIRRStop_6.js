e9d1ba1fd5ee6af643dbb8bdcb3ae774a898e916
Maria <maria.rodrig92@outlook.com>
2022-02-17 18:36:44 -0500

    added webmap

---
+++ mtabuswebapp/data/JamaicaLIRRStop_6.js
@@ -0,0 +1,8 @@
+var json_JamaicaLIRRStop_6 = {
+"type": "FeatureCollection",
+"name": "JamaicaLIRRStop_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "stop_name": "Jamaica" }, "geometry": { "type": "Point", "coordinates": [ -73.808529871584057, 40.69960816950195 ] } }
+]
+}
